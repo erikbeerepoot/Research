d997f7b2b954ae3acdfc32c701a6fefeb890c226
erikbeerepoot <erikbeerepoot@hush.com>
2013-08-20 14:43:44 -0400

    Added loadFeatures support function

--- ComparisonCode/CompareImagesByDescriptor.m
+++ ComparisonCode/CompareImagesByDescriptor.m
@@ -229,3 +229,14 @@ function [score,avgTrackLength] = CompareSURFDescriptors(im1,im2,feat1,feat2,doP
     score = surfMatches.numberOfMatches / maxNumFeatures;
     avgTrackLength = magnitudeSum / surfMatches.numberOfMatches;
 end
+
+function [feat nb dim]=loadFeatures(file)
+    fid = fopen(file, 'r');
+    dim=fscanf(fid, '%f',1);
+    if dim==1
+    dim=0;
+    end
+    nb=fscanf(fid, '%d',1);
+    feat = fscanf(fid, '%f', [5+dim, inf]);
+    fclose(fid);
+end
